1711db1409306e5c6d740be8fd99ce5815dc239d
Miranda Gavrin <mgavrin@mit.edu>
2015-03-16 19:46:40 +0000

    Started GUI

--- diamond.py
+++ diamond.py
@@ -1,3 +1,6 @@
+import pygame
+from pygame.locals import *
+
 #The board and its components
 class board:
     def __init__(self,numPlayers,AIs):
@@ -13,8 +16,9 @@ class board:
         self.hexagon=hexagon(self)
         if self.numPlayers==2:
             #define players
-            self.player1=player(self,1,"red",self.AIs[0])
-            self.player2=player(self,2,"blue",self.AIs[1])
+            self.player1=player(1,"red",self,self.AIs[0])
+            self.player2=player(2,"blue",self,self.AIs[1])
+            self.players=[self.player1,self.player2]
             #define triangles
             self.top=triangleCorner(self,"top",True,self.player1)
             self.bottom=triangleCorner(self,"bottom",True,self.player2)
@@ -32,6 +36,7 @@ class board:
             self.player1=player(self,1,"red",self.AIs[0])
             self.player2=player(self,2,"green",self.AIs[1])
             self.player3=player(self,3,"blue",self.AIs[2])
+            self.players=[self.player1,self.player2,self.player3]
             #define triangles
             self.top=triangleCorner(self,"top",True,self.player1)
             self.lowerLeft=triangleCorner(self,"lower left",True,self.player2)
@@ -59,6 +64,19 @@ class board:
             if triangle.inPlay:
                 for point in triangle.points:
                     point.contents=triangle.startPlayer.number
+        #This is a cheap hack
+        if self.numPlayers==2:
+            self.top.p33.contents=self.top.p00.contents
+            self.top.p30.contents=self.top.p00.contents
+            self.bottom.p00.contents=self.bottom.p30.contents
+            self.bottom.p03.contents=self.bottom.p30.contents
+        else:
+            self.top.p33.contents=self.top.p00.contents
+            self.top.p30.contents=self.top.p00.contents
+            self.lowerLeft.p33.contents=self.lowerLeft.p00.contents
+            self.lowerLeft.p30.contents=self.lowerLeft.p00.contents
+            self.lowerRight.p33.contents=self.lowerRight.p00.contents
+            self.lowerRight.p30.contents=self.lowerRight.p00.contents
         #handle the point list, position-->point dictionary, and connections
         self.allPoints=list(self.hexagon.points)
         self.pointPositions={}
@@ -148,5 +166,124 @@ class player:
         self.color=color
         self.board=board
         self.AI=AI
+        self.curMoveChain=[] #list of points in move, starting with initial location of moved piece
+
+    def useInput(self,event):
+        if event.type==KEYDOWN and event.key==K_RETURN:
+            if len(self.curMoveChain)>0:
+                self.curMoveChain[-1].contents=self.curMoveChain[0].contents
+                self.curMoveChain[0].contents=0
+        elif event.type==KEYDOWN and event.key==K_BACKSPACE:
+            if len(self.curMoveChain)>0:
+                self.curMoveChain=self.curMoveChain[:-1]
+        elif event.type==MOUSEBUTTONDOWN:
+            pos=event.pos
+            point=self.board.getNearestPoint(pos)
+            if len(self.curMoveChain)==0:
+                self.curMoveChain=[point]
+            elif self.curMoveChain[-1].canJumpTo(point):
+                self.curMoveChain.append(point)
+
+    def AIMove(self):
+        pass
+
+    #Current intended move procedure: click the piece you want to move,
+        #then click each circle on your path, then press enter when you're done.
+        #If you click somewhere wrong, press backspace to undo it.
+        #If you click somewhere illegal, nothing will happen.
+        #If you press Enter without having selected a valid move, nothing will happen.
+
+class screen: #the pygame screen and high-level "running the game" stuff
+    def __init__(self,board,xDim,yDim,players):
+        self.board=board
+        self.xDim=xDim
+        self.yDim=yDim
+        self.players=players
+        pygame.init()
+        self.background=pygame.image.load("blank background.png")
+        self.gameScreen=pygame.display.set_mode((xDim,yDim),0,32)
+        self.patternUnit=pygame.image.load("pattern unit.png")
+        self.backgroundColor=pygame.Color(175,175,175)
+        self.clock=pygame.time.Clock()
+        self.fps=36
+        self.font = pygame.font.SysFont('Arial', 25)
+        self.instructions=["Click the piece you want to move,","then each space in its path.","Press Enter when you are finished.","Press Backspace to undo a click."]
+        for i in range(0,len(self.instructions)):
+            self.gameScreen.blit(self.font.render(self.instructions[i], True, (0,0,255)), (20, 450+30*i))
+        self.curPlayer=self.players[0] #the player whose turn it is
+        self.running=True #the game has not been quit
+        self.win=False #nobody has won yet
+
+        self.mainloop() #this has to be the last thing in the init,
+        #because it isn't supposed to terminate until you end the session
+
+    def mainloop(self):
+        while self.running:
+            for player in self.players:
+                if not player.AI:
+                    self.getInput(player)
+                else:
+                    player.AIMove()
+                self.drawScreen()
+                self.checkWin()
+            self.clock.tick(self.fps)
+
+    def getInput(self,player):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                self.running=False
+                break
+            else:
+                player.useInput(event)
+
+    def drawScreen(self):
+        self.gameScreen.fill(self.backgroundColor)
+        colorDict={0:(255,255,255),1:(255,0,0),2:(0,255,0),3:(0,0,255)}
+        for pos in self.board.pointPositions:
+            colorNum=self.board.pointPositions[pos].contents
+            color=colorDict[colorNum]
+            pygame.draw.circle(self.gameScreen,color,pos,10,10) #piece or empty white circle 
+            pygame.draw.circle(self.gameScreen,(0,0,0),pos,11,1) #black border
+            #self.gameScreen.blit(self.patternUnit,pos)
+            neighbors=self.board.pointPositions[pos].neighbors
+            #TODO add edges
+        for i in range(0,len(self.instructions)):
+            self.gameScreen.blit(self.font.render(self.instructions[i], True, (0,0,255)), (20, 450+30*i))
+        pygame.display.flip()
+
+    def checkWin(self):
+        p1win=True
+        p2Win=True
+        if len(self.players)==3:
+            p3win=True
+        for point in self.players[0].endTri.points:
+            if point.contents!=1:
+                p1win=False
+                break
+        for point in self.players[1].endTri.points:
+            if point.contents!=2:
+                p2win=False
+                break
+        if len(self.players)==3:
+            p3win=True
+            for point in self.players[2].endTri.points:
+                if point.contents!=3:
+                    p3win=False
+                    break
+        winMessage=""
+        if p1win:
+            winMessage+="Player 1 wins! "
+        if p2win:
+            winMessage+="Player 2 wins! "
+        if len(self.players)==3 and p3win:
+            winMessage+="Player 3 wins!"
+        self.gameScreen.blit(self.font.render(winMessage, True, (0,0,255)), (300, 450))
+        
 
 test=board(2,[False,False])
+game=screen(test,500,1000,test.players)
+
+#Code provenance notes:
+    #Instructions and code for putting text on the screen were borrowed from
+    #http://stackoverflow.com/questions/19117062/how-to-add-text-into-a-pygame-rectangle .
